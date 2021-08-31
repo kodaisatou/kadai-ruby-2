7cd657eca08ca8e82de71b13289e9859c8bb9708
kota.s <koudai8314@gmail.com>
2021-08-31 11:57:29 +0000

    Lesson6課題

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(a,b)
+    num = 0
+    (a..b).each do |number|
+        num = number
+    if num % 3 == 0 && num % 5 == 0
+        puts 'FizzBuzz'
+    elsif num % 3 == 0
+        puts 'Fizz'
+    elsif num % 5 == 0
+        puts 'Buzz'
+    else puts num
+    end
+end
+end
+
+num_max = 100
+fizzbuzz(1,num_max)
