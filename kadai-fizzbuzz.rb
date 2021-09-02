292d472fb13a97a5f8a5bd4be423854d82d0fed7
kota.s <koudai8314@gmail.com>
2021-09-02 21:58:10 +0000

    Lesson6課題

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(a,b)
-    num = 0
-    (a..b).each do |number|
-        num = number
-    if num % 3 == 0 && num % 5 == 0
-        puts 'FizzBuzz'
-    elsif num % 3 == 0
-        puts 'Fizz'
-    elsif num % 5 == 0
-        puts 'Buzz'
-    else puts num
-    end
+  num = 0
+  (a..b).each do |number|
+    num = number
+  if num % 3 == 0 && num % 5 == 0
+    puts 'FizzBuzz'
+  elsif num % 3 == 0
+    puts 'Fizz'
+  elsif num % 5 == 0
+    puts 'Buzz'
+  else puts num
+  end
 end
 end
 
